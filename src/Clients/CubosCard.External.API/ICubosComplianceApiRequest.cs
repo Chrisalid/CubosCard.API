63e7d1364661b1478239eae892f8115e2a6146ae
Christopher Feitosa do Monte <ChristopherFeitosa17@hotmail.com>
2025-08-02 17:44:47 -0300

    feat: Adicionando Annotation para requisições

--- src/Clients/CubosCard.External.API/ICubosComplianceApiRequest.cs
+++ src/Clients/CubosCard.External.API/ICubosComplianceApiRequest.cs
@@ -5,13 +5,18 @@ namespace CubosCard.External.API;
 
 public interface ICubosComplianceApiRequest
 {
+    [Post("/auth/code")]
     Task<JsonResultExternalApi> AuthCode(string email, string password);
 
+    [Post("/auth/token")]
     Task<JsonResultExternalApi> AuthToken(string code);
-    
+
+    [Post("/auth/refresh")]
     Task<JsonResultExternalApi> AuthRefresh([Header("Authorization")] string token, string refreshToken);
 
+    [Post("/cpf/validate")]
     Task<JsonResultExternalApi> CpfValidate([Header("Authorization")] string token, string socialSecurity);
 
+    [Post("/cnpj/validate")]
     Task<JsonResultExternalApi> CnpjValidate([Header("Authorization")] string token, string taxNumber);
 }
